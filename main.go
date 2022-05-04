0c7cf62b2e4d84f1559c9519d05a9f40cb6b5d53
Mobile Data Books, LLC <constantine@mobiledatabooks.com>
2022-05-04 12:46:58 -0700

    go-smart-sqlite-cmd_v0.1_

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+// # Copyright 2022 Mobile Data Books, LLC. All rights reserved.
+// # Use of this source code is governed by a BSD-style
+// # license that can be found in the LICENSE file.
 import (
 	"database/sql"
 	"fmt"
